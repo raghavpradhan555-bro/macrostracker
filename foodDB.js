f839754a293897037b55bdf7113a872ce1e59f66
raghavpradhan555-bro <raghavpradhan555@gmail.com>
2026-04-17 23:11:04 +0530

    Add files via upload

---
+++ foodDB.js
@@ -0,0 +1,7 @@
+const foodDB = {
+  "egg": { calories: 78, protein: 6, carbs: 1, fats: 5 },
+  "bread": { calories: 80, protein: 3, carbs: 15, fats: 1 },
+  "rice": { calories: 130, protein: 2.5, carbs: 28, fats: 0.3 },
+  "chicken": { calories: 165, protein: 31, carbs: 0, fats: 3.6 },
+  "milk": { calories: 60, protein: 3.2, carbs: 5, fats: 3.3 }
+};
