8c004fa53ac25988cce6acf37b5257fbce280cbd
raghavpradhan555-bro <raghavpradhan555@gmail.com>
2026-04-24 15:34:23 +0530

    Update foodDB.js

--- foodDB.js
+++ foodDB.js
@@ -78,6 +78,75 @@ rasgulla: { calories: 125, protein: 3, carbs: 30, fats: 0 },
 tea: { calories: 40, protein: 1, carbs: 5, fats: 1 },
 coffee: { calories: 30, protein: 1, carbs: 4, fats: 1 },
 juice: { calories: 120, protein: 1, carbs: 28, fats: 0 },
-cola: { calories: 140, protein: 0, carbs: 35, fats: 0 }
+cola: { calories: 140, protein: 0, carbs: 35, fats: 0 },
 
+// 🍗 More protein foods
+omelette: { calories: 120, protein: 8, carbs: 1, fats: 9 },
+boiledegg: { calories: 70, protein: 6, carbs: 1, fats: 5 },
+friedegg: { calories: 90, protein: 6, carbs: 1, fats: 7 },
+chickenbreast: { calories: 165, protein: 31, carbs: 0, fats: 3 },
+chickenthigh: { calories: 209, protein: 26, carbs: 0, fats: 10 },
+chickenbiryani: { calories: 290, protein: 15, carbs: 35, fats: 10 },
+fishfry: { calories: 250, protein: 20, carbs: 5, fats: 15 },
+paneerbhurji: { calories: 300, protein: 18, carbs: 5, fats: 22 },
+
+// 🍚 More carbs / Indian foods
+jeerarice: { calories: 180, protein: 4, carbs: 30, fats: 5 },
+pulav: { calories: 250, protein: 6, carbs: 40, fats: 8 },
+vegfriedrice: { calories: 220, protein: 5, carbs: 35, fats: 6 },
+chickenfriedrice: { calories: 300, protein: 12, carbs: 40, fats: 10 },
+dalchawal: { calories: 350, protein: 12, carbs: 55, fats: 6 },
+curdrice: { calories: 200, protein: 6, carbs: 30, fats: 5 },
+
+// 🥞 Breakfast foods
+breadbutter: { calories: 150, protein: 3, carbs: 18, fats: 7 },
+breadjam: { calories: 140, protein: 3, carbs: 25, fats: 3 },
+cornflakes: { calories: 110, protein: 2, carbs: 25, fats: 1 },
+muesli: { calories: 250, protein: 8, carbs: 40, fats: 6 },
+pancake: { calories: 180, protein: 5, carbs: 30, fats: 5 },
+waffle: { calories: 220, protein: 6, carbs: 30, fats: 8 },
+
+// 🥦 More vegetables
+capsicum: { calories: 20, protein: 1, carbs: 5, fats: 0 },
+broccoli: { calories: 55, protein: 4, carbs: 11, fats: 0 },
+cabbage: { calories: 25, protein: 1, carbs: 6, fats: 0 },
+beetroot: { calories: 43, protein: 1.6, carbs: 10, fats: 0 },
+okra: { calories: 33, protein: 2, carbs: 7, fats: 0 },
+brinjal: { calories: 25, protein: 1, carbs: 6, fats: 0 },
+
+// 🍎 More fruits
+pineapple: { calories: 82, protein: 1, carbs: 22, fats: 0 },
+papaya: { calories: 55, protein: 1, carbs: 14, fats: 0 },
+kiwi: { calories: 42, protein: 1, carbs: 10, fats: 0 },
+pear: { calories: 100, protein: 1, carbs: 27, fats: 0 },
+pomegranate: { calories: 83, protein: 2, carbs: 19, fats: 1 },
+
+// 🥜 More nuts & healthy fats
+walnut: { calories: 26, protein: 0.6, carbs: 0.5, fats: 2.6 },
+pistachio: { calories: 4, protein: 0.2, carbs: 0.3, fats: 0.3 },
+peanutbutter: { calories: 95, protein: 4, carbs: 3, fats: 8 },
+flaxseed: { calories: 55, protein: 2, carbs: 3, fats: 4 },
+chia: { calories: 60, protein: 2, carbs: 5, fats: 4 },
+
+// 🍔 More fast food
+fries: { calories: 365, protein: 4, carbs: 63, fats: 17 },
+sandwich: { calories: 250, protein: 12, carbs: 30, fats: 10 },
+clubsandwich: { calories: 350, protein: 18, carbs: 35, fats: 15 },
+shawarma: { calories: 400, protein: 20, carbs: 35, fats: 18 },
+pasta: { calories: 220, protein: 8, carbs: 43, fats: 1 },
+whitepasta: { calories: 300, protein: 10, carbs: 45, fats: 10 },
+
+// 🍬 More sweets
+gulabjamun: { calories: 150, protein: 2, carbs: 25, fats: 5 },
+kheer: { calories: 200, protein: 6, carbs: 30, fats: 6 },
+halwa: { calories: 250, protein: 4, carbs: 35, fats: 10 },
+icecream: { calories: 200, protein: 3, carbs: 25, fats: 10 },
+chocolate: { calories: 150, protein: 2, carbs: 20, fats: 8 },
+
+// 🥤 More drinks
+lassi: { calories: 180, protein: 6, carbs: 20, fats: 6 },
+buttermilk: { calories: 60, protein: 3, carbs: 5, fats: 3 },
+proteinshake: { calories: 200, protein: 25, carbs: 10, fats: 5 },
+smoothie: { calories: 150, protein: 3, carbs: 30, fats: 2 },
+energydrink: { calories: 110, protein: 0, carbs: 28, fats: 0 }
 };
